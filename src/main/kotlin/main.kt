0c44e5bcc5f662cfb19c894f7ff2269a98030296
Pedro Ferreira <pedroferreiracjr@gmail.com>
2020-11-01 20:56:40 -0300

    #17 formatação de strings implementado

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,40 +1,71 @@
 /**
- *  Variáveis Mutáveis e Imutáveis
- *      var e val;
+ *  Formatação de Strings
+ *
  * */
 fun main(args: Array<String>) {
-    variavel_mutavel()
-    variavel_imutavel()
+    var resultado: String = concatenacao_strings()
+    println(resultado)
+    resultado = interpolacao_strings()
+    println(resultado)
+    resultado = interpolacao_strings_2()
+    println(resultado)
+    resultado = interpolacao_strings_multiplas_linhas()
+    println(resultado)
 }
 
 /**
- * A variável declarada com a keyword var, define uma
- *  variável mutável. Aquela que onde a reatribuição
- *  é permitida.
- *  @return: Unit
+ * O operador + quando utilizado com strings gera
+ *  uma string resultando da junção dos elementos fornecidos.
+ *  @return: String
  */
-fun variavel_mutavel() : Unit {
-    println("iniciando método variavel_mutavel()")
-    var status: Boolean = false
-    if (!status) {
-        status = true
-    }
-    println("terminando método variavel_mutavel()")
+fun concatenacao_strings(): String {
+    val firstName: String = "Pedro"
+    val lastName: String = "Ferreira"
+
+    return firstName + " " + lastName
 }
 
 /**
- * A variável declarada com a keyword val, define uma
- *  variável imutável. Essa variável só recebe uma única
- *  atribuição.
- *  @return: Unit
+ * A interpolação de variáveis é possivel utilizando
+ *  o operador cifrão ($) dentro de uma string, juntamente
+ *  com uma variável em escopo visível, que é
+ *  interpolada dentro da string resultante.
+ *  @return: String
  */
-fun variavel_imutavel() : Unit {
-    println("iniciando método variavel_imutavel()")
-    val status: Boolean = false
-    if (!status) {
-        // A linha abaixo, se descomentada, gera um erro
-        // de compilação
-        //status = true
-    }
-    println("terminando método variavel_imutavel()")
+fun interpolacao_strings(): String {
+    val firstName: String = "Pedro"
+    val lastName: String = "Ferreira"
+
+    return "$firstName $lastName"
+}
+
+/**
+ * Outra possibilidade de interpolação de variáveis em
+ *  uma template string é através do operador cifrão ($)
+ *  mais a utilização de chaves (${})
+ *  @return: String
+ */
+fun interpolacao_strings_2(): String {
+    val firstName: String = "Pedro"
+    val lastName: String = "Ferreira"
+
+    return "$firstName $lastName - ${firstName[0]}${lastName[0]}"
+}
+
+/**
+ * O kotlin possui uma facilidade para representação
+ *  de variáveis string de tamanho grande. Que é o
+ *  operador três aspas duplas (""") para iniciar a declaração
+ *  da string e três aspas duplas (""") para terminar
+ *  a declaração da string de multiplas linhas.
+ *  @return: String
+ */
+fun interpolacao_strings_multiplas_linhas(): String {
+    val firstName: String = "Pedro"
+    val lastName: String = "Ferreira"
+    return """
+        $firstName $lastName - ${firstName[0]}${lastName[0]}
+        A linguagem de programação Kotlin é realmente muito interessante.
+        Obrigado por sua colaboração!
+    """.trimIndent()
 }
