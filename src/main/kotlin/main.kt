26dd424154f108e6fc2afb6e6b3f6aed3a85763c
Pedro Ferreira <pedroferreiracjr@gmail.com>
2020-11-01 20:13:16 -0300

    #15 tipos de dados implementado

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,5 +1,33 @@
+/**
+ *  Types:
+ *      Byte (1 byte),
+ *      Char,
+ *      Short (2 bytes),
+ *      Int (4 bytes),
+ *      Long (8 bytes),
+ *      Float (4 bytes),
+ *      Double (8 bytes),
+ *      Boolean,
+ *      String,
+ *      Void
+ * */
 fun main(args: Array<String>) {
-    var name: String = "Pedro Ferreira"
-    var age: Int = 28
-    println("Hi, my name is $name and i am $age years old")
+    // representa um caráctere
+    val c: Char = 'A'
+
+    // representa inteiros
+    val b: Byte = Byte.MIN_VALUE
+    val s: Short = Short.MIN_VALUE
+    val i: Int = Int.MIN_VALUE
+    val l: Long = Long.MIN_VALUE
+
+    // representa números de ponto flutuante
+    val f: Float = Float.MIN_VALUE
+    val d: Double = Double.MIN_VALUE
+
+    // representa valores lógicos, true e false
+    val bln: Boolean = false
+
+    // representa uma cadeia de caráctere
+    val str: String = "Pedro"
 }
