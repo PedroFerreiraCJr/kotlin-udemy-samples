aba2d6b4d4ff202be76b2d169f6f75c526040d60
Pedro Ferreira <pedroferreiracjr@gmail.com>
2020-11-01 23:26:36 -0300

    #19 funções de linha única implementada

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,79 +1,77 @@
-import java.text.DecimalFormat
-
 /**
- *  Criando funções
- *      As funções são declaradas com a keyword fun.
- *      O objetivo da função é declarar um trecho
- *      de código que é reutilizável, tornando o código
- *      mais fácil de dar manutenção em caso de alterações.
+ *  Criando funções de linha única
+ *      O operador de atribuição é sobrecarregado para essa operação.
  * */
 fun main(args: Array<String>) {
-    // define o valor inicial do salário do funcionário
-    val salario: Double = 4_200.00
-    println("Total do salário do funcionário Pedro é: $salario")
+    val a: Double = 10.0
+    val b: Double = 20.0
 
-    // declara variável para receber o calculo do bonus salarial
-    var total: Double
+    println("calculando a soma de a ($a) mais b ($b), resultado: ${soma(a, b)}")
+    println("calculando a subtração de a ($a) menos b ($b), resultado: ${subtracao(a, b)}")
+    println("calculando a multiplicação de a ($a) por b ($b), resultado: ${multiplicacao(a, b)}")
+    println("calculando a divisão de a ($a) por b ($b), resultado: ${divisao(a, b)}")
 
-    // calcula o bonus salarial com a função de valor fixo
-    total = calcular_bonus_salarial(salario)
-    println("O total do salário do funcionário Pedro com bônus de 30% é: ${DecimalFormat("#,###.00").format(total)}")
-
-    // calcula o bonus salarial com a função que recebe como parâmetro o percentual de bonus
-    total = calcular_bonus_salarial(salario, 0.5)
-    println("O total do salário do funcionário Pedro com bônus de 50% e com isenção de imposto é: ${DecimalFormat("#,###.00").format(total)}")
-
-    // calcula o bonus salarial com a função que recebe como parâmetro o percentual de bonus
-    total = calcular_bonus_salarial(salario, 0.5, false)
-    println("O total do salário do funcionário Pedro com bônus de 50% sem isenção de imposto é: ${DecimalFormat("#,###.00").format(total)}")
+    println("O valor de a ($a) é par ? ${sim_nao(isPair(a.toLong()))}")
+    println("As letras iniciais do seu nome são: ${abbr("Pedro", "Junior")}")
 }
 
 /**
- * A declaração de uma função se dá utilizando a keyword fun.
- * A assinatura da função é composta de:
- *  1.  identificador da função, que é obrigatória;
- *  2.  parâmetros são opcionais;
- *  3.  tipo de valor retorndo pela função quando invocada, ou
- *      em caso de função que não retorna valor, utilizar
- *      os tipos Unit ou Void;
- * Exemplo de função de calculo de bonus salarial.
- *
- * @param: salario: Double - salário do funcionário
+ * A declaração de função de linha única se dá como
+ *  a função a seguir, que utiliza o operador igual (=).
+ * A seguinte função calcula a soma de dois números.
+ * @param: a: Double - primeiro operando da soma
+ * @param: b: Double - segundo operando da soma
+ * @return: Double - retorna a+b
  */
-fun calcular_bonus_salarial(salario: Double): Double {
-    println("iniciando calcular_bonus_salarial")
-    val total: Double = salario + (0.3 * salario)
-    println("termino calcular_bonus_salarial")
-    return total
-}
+fun soma(a: Double, b: Double) : Double = a + b
 
 /**
- * A declaração de uma função se dá utilizando a keyword fun.
- * A seguinte função é declarada como uma sobrecarga da função
- *  calcular_bonus_salarial, e nessa versão, recebe como
- *  parâmetro o valor do salário, e percentual de incremento.
- * A função ainda recebe um parâmetro opcional com valor default
- *  definido para true, que indica se é necessário calcular o valor
- *  do imposto e subtrair do salario.
- * Exemplo de função de calculo de bonus salarial.
- *
- * @param: salario: Double - salario inicial
- * @param: percent: Double - percentual de incremento do bonus salarial
- * @param: semimposto: Boolean - flag que indica se deve ser calculado o
- *  imposto sobre o salário do funcionário
- * @return: Double, total do salario calculado com o bonus
+ * A declaração de função de linha única se dá como
+ *  a função a seguir, que utiliza o operador de atribuição (=).
+ * A seguinte função calcula a substração de dois números.
+ * @param: a: Double - primeiro operando da subtração
+ * @param: b: Double - segundo operando da subtração
+ * @return: Double - retorna a-b
  */
-fun calcular_bonus_salarial(salario: Double, percent: Double, semimposto: Boolean = true): Double {
-    println("iniciando calcular_bonus_salarial")
-    if (semimposto) {
-        val total: Double = salario + (percent * salario)
-        println("termino calcular_bonus_salarial")
-       return total
-    }
+fun subtracao(a: Double, b: Double) : Double = a - b
 
-    val imposto: Double = 0.25
-    val total: Double = salario + (percent * salario) - salario * imposto
+/**
+ * A declaração de função de linha única se dá como
+ *  a função a seguir, que utiliza o operador de atribuição (=).
+ * A seguinte função calcula a multiplicação de dois números.
+ * @param: a: Double - primeiro operando da multiplicação
+ * @param: b: Double - segundo operando da multiplicação
+ * @return: Double - retorna a*b
+ */
+fun multiplicacao(a: Double, b: Double) : Double = a * b
 
-    println("termino calcular_bonus_salarial")
-    return total
-}
+/**
+ * A declaração de função de linha única se dá como
+ *  a função a seguir, que utiliza o operador de atribuição (=).
+ * A seguinte função calcula a divisão de dois números.
+ * @param: a: Double - primeiro operando da divisão
+ * @param: b: Double - segundo operando da divisão
+ * @return: Double - retorna a/b
+ */
+fun divisao(a: Double, b: Double) : Double = a / b
+
+/**
+ * A declaração de função de linha única se dá como
+ *  a função a seguir, que utiliza o operador de atribuição (=).
+ * A seguinte função verifica se um número é par.
+ * @param: value: Long - valor a verificar se é par
+ * @return: Boolean - retornar verdadeiro se o valor é par
+ */
+fun isPair(value: Long) : Boolean = (value % 2) == 0L
+
+/**
+ * A declaração de função de linha única se dá como
+ *  a função a seguir, que utiliza o operador de atribuição (=).
+ * A seguinte função retorna as letras iniciais do nome próprio.
+ * @param: nome: String - nome
+ * @param: lastname: String - sobrenome
+ * @return: String - iniciais do nome e sobrenome
+ */
+fun abbr(name: String, lastname: String) : String = "${name[0].toUpperCase()}${lastname[0].toUpperCase()}"
+
+fun sim_nao(value: Boolean) : String = if (value) "Sim" else "Não"
